3a6e52b87bec734d4422dd6a9d69cd235ffe3c1c
iamharishshettymerdi <harisha@litmus7.com>
2022-05-02 12:27:52 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import styled from "styled-components";
 
+const Header = styled.h1`
+  color: red;
+
+  @media (max-width: 480px) {
+    font-size: 24px;
+    color: green;
+  }
+`;
+const Hero = styled(Header)`
+  color: blue;
+  font-size: 100px;
+`;
 const App = () => {
-  return <h1>Hello React!</h1>;
+  return (<React.Fragment>
+            <Hero>Hero element </Hero>
+            <Header>Hello React!</Header>
+          </React.Fragment>);
 };
+
+
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
